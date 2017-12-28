57ceb9bb31c2c1e36014b3420ad196d73ef1f345
Aniket Pandey <aniketp@iitk.ac.in>
2017-12-29 02:00:15 +0530

    Create initial generator

---
+++ main.lua
@@ -0,0 +1,78 @@
+-- Created Using Intellij IDEA
+-- Autor: Aniket Pandey
+-- Date: 29/12/17
+-- Time: 12:02 AM
+--
+
+wordLength = arg[1]
+queries = tostring(arg[2])
+queryLength = #queries
+
+mainTable = {}
+
+if arg[2] == nil then
+    print("Usage: lua main.lua <word-length> <allowed-characters>\n")
+end
+
+-- Convert character query to a table form
+wordTable = {}
+queries:gsub(".",function(c) table.insert(wordTable,c) end)
+
+-- Number of possible words
+maxCombo = math.pow(queryLength, wordLength)
+
+
+-- Insert combinations in the Main Table
+for i=0, math.huge do
+
+    -- Counting table length
+    local count = 0
+    for _ in pairs(mainTable) do
+        count = count + 1
+    end
+
+    -- We have all possible combinations
+    if count == maxCombo then
+        print("Wordlist Completed")
+
+        -- Write the words to a a file
+        local wordlist = io.open("words.txt", "a")
+
+        io.output(wordlist)
+        io.write(string.format("Words generated for length %d and characters \'%s\'\n", wordLength, queries))
+
+        for _, value in pairs(mainTable) do
+            io.write(value, "\n")
+        end
+        io.write("\n\n")
+        io.close(wordlist)
+
+        os.execute("cat words.txt")
+
+        break
+    else
+
+        -- Chose any 'wordlength' characters from wordTable
+        local tempQuery = ''
+        math.randomseed(os.time())
+        for i=1, wordLength do
+            local letter = math.random(queryLength)
+            tempQuery = tempQuery .. queries:sub(letter,letter)
+        end
+
+        -- Check if tempQuery is in mainTable or not
+        local condition = false
+        for _, value in pairs(mainTable) do
+            if tempQuery == value then
+                condition = true; break
+            end
+        end
+
+        -- Insert the value to mainTable
+        if condition == false then
+            table.insert(mainTable, tempQuery)
+        end
+
+    end
+
+end
