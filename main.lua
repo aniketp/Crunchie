d32a8edc5704fea49c408e78bac31852c5ed7e02
Aniket Pandey <aniketp@iitk.ac.in>
2017-12-29 15:12:45 +0530

    Bring driver function within scope

--- main.lua
+++ main.lua
@@ -10,70 +10,93 @@ queryLength = #queries
 
 mainTable = {}
 
-if arg[2] == nil then
-    print("Usage: lua main.lua <word-length> <allowed-characters>\n")
+printAllWords = function(wordTable, prefix, queryLength, wordLength)
+    if wordLength == 0 then
+        print(prefix)
+    end
+
+    for i=1, queryLength do
+        prefix = prefix .. tostring(wordTable[i])
+        printAllWords(wordTable, prefix, queryLength, wordLength-1)
+    end
 end
 
--- Convert character query to a table form
-wordTable = {}
-queries:gsub(".",function(c) table.insert(wordTable,c) end)
+driver = function()
+    -- Convert character query to a table form
+    local wordTable = {}
+    queries:gsub(".",function(c) table.insert(wordTable,c) end)
 
--- Number of possible words
-maxCombo = math.pow(queryLength, wordLength)
+    -- Number of possible words
+    local maxCombo = math.pow(queryLength, wordLength)
 
 
--- Insert combinations in the Main Table
-for i=0, math.huge do
+    -- Insert combinations in the Main Table
+    for i=0, math.huge do
 
-    -- Counting table length
-    local count = 0
-    for _ in pairs(mainTable) do
-        count = count + 1
-    end
-
-    -- We have all possible combinations
-    if count == maxCombo then
-        print("Wordlist Completed, writing to file \'words.txt\'")
-
-        -- Write the words to a a file
-        local wordlist = io.open("words.txt", "a")
-
-        io.output(wordlist)
-        io.write(string.format("Words generated for length %d and characters \'%s\'\n", wordLength, queries))
-
-        for _, value in pairs(mainTable) do
-            io.write(value, "\n")
-        end
-        io.write("\n\n")
-        io.close(wordlist)
-
-        --os.execute("cat words.txt")
-
-        break
-    else
-
-        -- Chose any 'wordlength' characters from wordTable
-        local tempQuery = ''
-        math.randomseed(os.time())
-        for i=1, wordLength do
-            local letter = math.random(queryLength)
-            tempQuery = tempQuery .. queries:sub(letter,letter)
+        -- Counting table length
+        local count = 0
+        for _ in pairs(mainTable) do
+            count = count + 1
         end
 
-        -- Check if tempQuery is in mainTable or not
-        local condition = false
-        for _, value in pairs(mainTable) do
-            if tempQuery == value then
-                condition = true; break
+        -- We have all possible combinations
+        if count == maxCombo then
+            print("Wordlist Completed, writing to file \'words.txt\'")
+
+            -- Write the words to a a file
+            local wordlist = io.open("words.txt", "a")
+
+            io.output(wordlist)
+            io.write(string.format("Words generated for length %d and characters \'%s\'\n", wordLength, queries))
+
+            for _, value in pairs(mainTable) do
+                io.write(value, "\n")
             end
-        end
+            io.write("\n\n")
+            io.close(wordlist)
 
-        -- Insert the value to mainTable
-        if condition == false then
-            print(tempQuery)
-            table.insert(mainTable, tempQuery)
+            --os.execute("cat words.txt")
+
+            break
+        else
+
+            -- Chose any 'wordlength' characters from wordTable
+            local tempQuery = ''
+            math.randomseed(os.time())
+            for i=1, wordLength do
+                local letter = math.random(queryLength)
+                tempQuery = tempQuery .. queries:sub(letter,letter)
+            end
+
+            -- Check if tempQuery is in mainTable or not
+            local condition = false
+            for _, value in pairs(mainTable) do
+                if tempQuery == value then
+                    condition = true; break
+                end
+            end
+
+            -- Insert the value to mainTable
+            if condition == false then
+                print(tempQuery)
+                table.insert(mainTable, tempQuery)
+
+                -- Use this code after recursion issue is fixed
+                -- local prefix = ''
+                -- printAllWords(wordTable, prefix, queryLength, wordLength)
+            end
+
         end
 
     end
+end
 
+if arg[2] == nil then
+    print("Usage: lua main.lua <word-length> <allowed-characters>\n")
+elseif #arg[2] > 4 then
+    io.write("Input query too large, can cause performance issues\nDo you want to continue?(Y/n)")
+    local dec = io.read(); if (dec == '' or dec:lower() == 'y') then driver() else print("Program terminated")
+    end
+else
+    driver()
 end
